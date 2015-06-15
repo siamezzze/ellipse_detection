bfe58f3bdd9abbc81070a6d5f1711d2bffa9c68c
siamezzze <siamezzze@gmail.com>
2015-06-15 05:17:15 +0300

    - Two main working functions simplified (I hope)

--- main.cpp
+++ main.cpp
@@ -81,6 +81,14 @@ vector<vector<vector<short> > > store_hiearchical_pyramid(Mat& edgesM, int min_s
     return pyramid;
 }
 
+bool compr(pair<ellipse_data, int> a, pair<ellipse_data, int> b) {
+    return a.second > b.second;
+}
+
+bool compr2(pair<ellipse_data, int> a, pair<ellipse_data, int> b) {
+    return a.second < b.second;
+}
+
 inline bool can_belong_to_ellipse(int x, int y, ellipse_data e, double eps = 0.1) //for cleaning the image
 {
     double _x = cos(e.orient) * (x - e.x0) + sin(e.orient) * (y - e.y0);
@@ -89,104 +97,106 @@ inline bool can_belong_to_ellipse(int x, int y, ellipse_data e, double eps = 0.1
     return abs(eqt - 1) < eps;
 }
 
-
-//First search (should be made on the minimized image)
-vector<pair<ellipse_data, int> > hough_transform(vector<vector<short> > data, int min_vote = 1, int min_dist = 0)
-{
-    int xstart = 0;
-    int xfin = (int)data.size();
-    int ystart = 0;
-    int yfin = (int)data[0].size();
-    vector<pair<ellipse_data,int> > res;
+void voting(vector<pair<ellipse_data, int> > &ellipses, const vector<vector<short> > &data, pair<int, int> xbounds,
+            pair<int, int> ybounds, int min_dist, int min_vote, pair<int, int> p0, pair<int, int> p1) {
     int maxlen = cvRound(sqrt(data.size() * data.size() + data[0].size() * data[0].size()));
-
     int acc[maxlen];
     for (int i = 0; i < maxlen; ++i) { //clear the accumulator array
         acc[i] = 0;
     }
+    int x1 = p0.first;
+    int y1 = p0.second;
+    int x2 = p1.first;
+    int y2 = p1.second;
 
-    for (int y1 = ystart; y1 < yfin; y1++)
+    pair<int, int> center = compute_center(x1, y1, x2, y2); //get the center point of the ellipse
+
+    int x0 = center.first;
+    int y0 = center.second;
+
+    double a_sq = major_axis_length_sq(x1, y1, x2, y2); //major axis (half)
+    double a = sqrt(a_sq);
+
+    if (a < min_dist)
+        return;
+
+    double orient = orientation(x1, y1, x2, y2); //ellipse can be rotated
+
+
+    for (int y = ybounds.first; y < ybounds.second; y++) {
+        for (int x = xbounds.first; x < xbounds.second; x++) //point of the ellipse (x,y)
+        {
+            if (((x == x1) && (y == y1)) ||
+                ((x == x2) && (y == y2)) ||
+                (data[y][x] == 0)) //the same as one of the side points or not a boundary point
+                continue;
+
+            double d_sq = distance_square(x, y, x0, y0);
+            double d = sqrt(d_sq); //distance from the center
+
+            if ((d < min_dist) || (d > a)) //if it's too small - it is probably a line
+                //if bigger than the major axis - not an ellipse point
+                continue;
+
+
+            double f_sq = min(distance_square(x, y, x1, y1),
+                              distance_square(x, y, x2, y2)); //distance to the nearest side point
+            double costau = (a_sq + d_sq - f_sq) / (2 * a * d);
+            double costau_sq = costau * costau;
+            if (costau_sq > 1) {
+                costau_sq = 1;
+            }
+            if (costau_sq < 0) {
+                costau_sq = 0;
+            }
+            double sintau_sq = 1 - costau_sq;
+            double b_sq = (a_sq * d_sq * sintau_sq) / (a_sq - d_sq * costau_sq);
+            int b = cvRound(sqrt(b_sq));
+            if (b > min_dist) { //voting
+                acc[b] += data[y][x];
+            }
+        }
+    }
+    //here we get the most-likely minor axis length of the assumed ellipse and how many points would such an ellipse contain
+    int maxlength = 0;
+    int maxvote = 0;
+    for (int k = 0; k < maxlen; ++k) {
+        if (acc[k] >= maxvote) {
+            maxvote = acc[k];
+            maxlength = k;
+        }
+        //acc[k] = 0;
+    }
+
+    if (maxvote > min_vote) {
+        ellipses.push_back(make_pair(ellipse_data(x0, y0, a, maxlength, orient, x1, y1, x2, y2), maxvote));
+    }
+}
+
+//First search (should be made on the minimized image)
+vector<pair<ellipse_data, int> > hough_transform(vector<vector<short> > &data, int min_vote = 1, int min_dist = 0)
+{
+    pair<int, int> xbounds = make_pair(0, (int) data.size());
+    pair<int, int> ybounds = make_pair(0, (int) data[0].size());
+    vector<pair<ellipse_data, int> > res = vector<pair<ellipse_data, int> >();
+
+    for (int y1 = ybounds.first; y1 < ybounds.second; y1++)
     {
-        for (int x1 = xstart; x1 < xfin; x1 ++) //point (x1, y1) - side point of major axis
+        for (int x1 = xbounds.first; x1 < xbounds.second; x1++) //point (x1, y1) - side point of major axis
         {
             if (data[y1][x1] == 0) //not a boundary point
                 continue;
 
 
-            for (int y2 = y1; y2 < yfin; y2++)
+            for (int y2 = y1; y2 < ybounds.second; y2++)
             {
-                for (int x2 = 0; x2 < xfin; x2 ++) //point (x2, y2) - side point of major axis
+                for (int x2 = xbounds.first; x2 < xbounds.second; x2++) //point (x2, y2) - side point of major axis
                 {
                     if (((y2 == y1) && (x2 <= x1)) ||
                         (data[y2][x2] == 0)) //already been selected OR not a boundary point
                         continue;
 
-                    pair<int, int> center = compute_center(x1, y1, x2, y2); //get the center point of the ellipse
-
-                    int x0 = center.first;
-                    int y0 = center.second;
-
-                    double a_sq = major_axis_length_sq(x1, y1, x2, y2); //major axis (half)
-                    double a = sqrt(a_sq);
-
-                    if (a < min_dist)
-                        continue;
-
-                    double orient = orientation(x1, y1, x2, y2); //ellipse can be rotated
-
-
-                    for (int y = ystart; y < yfin; y++)
-                    {
-                        for (int x = xstart; x < xfin; x++) //point of the ellipse (x,y)
-                        {
-                            if (((x == x1) && (y == y1)) ||
-                                ((x == x2) && (y == y2)) ||
-                                (data[y][x] == 0)) //the same as one of the side points or not a boundary point
-                                continue;
-
-                            double d_sq = distance_square(x, y, x0, y0);
-                            double d = sqrt(d_sq); //distance from the center
-
-                            if ((d < min_dist) || (d > a)) //if it's too small - it is probably a line
-                                //if bigger than the major axis - not an ellipse point
-                                continue;
-
-
-                            double f_sq = min(distance_square(x, y, x1, y1),
-                                              distance_square(x, y, x2, y2)); //distance to the nearest side point
-                            double costau = (a_sq + d_sq - f_sq) / (2 * a * d);
-                            double costau_sq = costau * costau;
-                            if (costau_sq > 1) {
-                                costau_sq = 1;
-                            }
-                            if (costau_sq < 0)
-                            {
-                                costau_sq = 0;
-                            }
-                            double sintau_sq = 1 - costau_sq;
-                            double b_sq = (a_sq * d_sq * sintau_sq) / (a_sq - d_sq * costau_sq);
-                            int b = cvRound(sqrt(b_sq));
-                            if (b > min_dist) { //voting
-                                acc[b] += data[y][x];
-                            }
-                        }
-                    }
-                    //here we get the most-likely minor axis length of the assumed ellipse and how many points would such an ellipse contain
-                    int maxlength = 0;
-                    int maxvote = 0;
-                    for (int k = 0; k < maxlen; ++k) {
-                        if (acc[k] >= maxvote)
-                        {
-                            maxvote = acc[k];
-                            maxlength = k;
-                        }
-                        acc[k] = 0;
-                    }
-
-                    if (maxvote > min_vote )
-                    {
-                        res.push_back(make_pair(ellipse_data(x0, y0, a, maxlength, orient, x1, y1, x2, y2), maxvote));
-                    }
+                    voting(res, data, xbounds, ybounds, min_dist, min_vote, make_pair(x1, y1), make_pair(x2, y2));
                 }
             }
         }
@@ -195,26 +205,53 @@ vector<pair<ellipse_data, int> > hough_transform(vector<vector<short> > data, in
     return res;
 }
 
-
-inline bool is_ellipse_point(int x, int y, ellipse_data e, double eps = 0.1)
+ellipse_data relocate_ellipse(vector<vector<short> > &data, ellipse_data prev, int min_vote = 1, int min_dist = 0)
 {
-    double _x = cos(e.orient) * (x - e.x0) + sin(e.orient) * (y - e.y0);
-    double _y = sin(e.orient) * (x - e.x0) - cos(e.orient) * (y - e.y0);
-    double eqt = (_x * _x)/(e.a*e.a) + (_y * _y)/(e.b*e.b);
-    return abs(eqt - 1) < eps;
+    //The area where we search for ellipse points (basically, all the image)
+    pair<int, int> xbounds = make_pair(0, (int) data.size());
+    pair<int, int> ybounds = make_pair(0, (int) data[0].size());
+    vector<pair<ellipse_data, int> > res;
+
+    int maxlen = cvRound(sqrt(data.size() * data.size() + data[0].size() * data[0].size()));
+
+    int acc[maxlen]; //TODO: Really, MUCH less since we know in what range it should be. Fix.
+    for (int i = 0; i < maxlen; ++i) { //clear the accumulator array
+        acc[i] = 0;
+    }
+
+
+    //This time, we search for side points only as far as +- pixel from previously found ones
+    //Okay, +-2 pixels for a little bit higher accuracy (I believe).
+    //TODO: Check. If everything else is correct, there should be +-1 pixel anyway.
+    int sidex1_start = max(xbounds.first, 2 * prev.x1 - 2);
+    int sidex1_fin = min(xbounds.second, 2 * prev.x1 + 2);
+    int sidex2_start = max(xbounds.first, 2 * prev.x2 - 2);
+    int sidex2_fin = min(xbounds.second, 2 * prev.x2 + 2);
+    int sidey1_start = max(ybounds.first, 2 * prev.y1 - 2);
+    int sidey1_fin = min(ybounds.second, 2 * prev.y1 + 2);
+    int sidey2_start = max(ybounds.first, 2 * prev.y2 - 2);
+    int sidey2_fin = min(ybounds.second, 2 * prev.y2 + 2);
+
+    for (int y1 = sidey1_start; y1 <= sidey1_fin; y1++) {
+        for (int x1 = sidex1_start; x1 <= sidex1_fin; x1++) //point (x1, y1) - side point of major axis
+        {
+            if (data[y1][x1] == 0)
+                continue;
+
+            for (int y2 = sidey2_start; y2 <= sidey2_fin; y2++) {
+                for (int x2 = sidex2_start; x2 <= sidex2_fin; x2++) //point (x2, y2) - side point of major axis
+                {
+                    if (((y2 == y1) && x2 <= x1) || (data[y2][x2] == 0))
+                        continue;
+
+                    voting(res, data, xbounds, ybounds, min_dist, min_vote, make_pair(x1, y1), make_pair(x2, y2));
+                }
+            }
+        }
+    }
+    pair<ellipse_data, int> e = *max_element(res.begin(), res.end(), compr2);
+    return e.first;
 }
-
-bool compr(pair<ellipse_data,int> a, pair<ellipse_data,int> b)
-{
-    return a.second > b.second;
-}
-
-bool compr2(pair<ellipse_data,int> a, pair<ellipse_data,int> b)
-{
-    return a.second < b.second;
-}
-
-
 
 //Have no use for this one, though it is supposed to deal with duplucates nearby
 vector<pair<ellipse_data,int> > remove_duplicates(vector<vector<short> > data, vector<pair<ellipse_data,int> > ellipses, int thr = 20)
@@ -234,7 +271,7 @@ vector<pair<ellipse_data,int> > remove_duplicates(vector<vector<short> > data, v
                 continue;
             for (int i = 0; i < res.size(); i++)
             {
-                if (is_ellipse_point(x,y,res[i].first))
+                if (can_belong_to_ellipse(x, y, res[i].first))
                 {
                     //cout << i << " " << x << " " << y << endl;
                     res[i].second+=data[x][y];
@@ -311,134 +348,6 @@ void draw_ellipse(Mat & canvas, ellipse_data elp, int step = 0)
 }
 
 //end of debug output functions
-
-ellipse_data relocate_ellipse(vector<vector<short> > data, ellipse_data prev, int min_vote = 1, int min_dist = 0)
-{
-    //The area where we search for ellipse points (basically, all the image)
-    int xstart = 0;
-    int xfin = (int)data[0].size() - 1;
-    int ystart = 0;
-    int yfin = (int)data.size() - 1;
-    vector<pair<ellipse_data,int> > res;
-
-    int maxlen = cvRound(sqrt(data.size() * data.size() + data[0].size() * data[0].size()));
-
-    int acc[maxlen]; //TODO: Really, MUCH less since we know in what range it should be. Fix.
-    for (int i = 0; i < maxlen; ++i) { //clear the accumulator array
-        acc[i] = 0;
-    }
-
-
-    //This time, we search for side points only as far as +- pixel from previously found ones
-    //Okay, +-2 pixels for a little bit higher accuracy (I believe).
-    //TODO: Check. If everything else is correct, there should be +-1 pixel anyway.
-    int sidex1_start = max(xstart, 2 * prev.x1 - 2);
-    int sidex1_fin = min(xfin, 2 * prev.x1 + 2);
-    int sidex2_start = max(xstart, 2 * prev.x2 - 2);
-    int sidex2_fin = min(xfin, 2 * prev.x2 + 2);
-    int sidey1_start = max(ystart, 2 * prev.y1 - 2);
-    int sidey1_fin = min(yfin, 2 * prev.y1 + 2);
-    int sidey2_start = max(ystart, 2 * prev.y2 - 2);
-    int sidey2_fin = min(yfin, 2 * prev.y2 + 2);
-
-    for (int y1 = sidey1_start; y1 <= sidey1_fin; y1++)
-    {
-        for (int x1 = sidex1_start; x1 <= sidex1_fin; x1++) //point (x1, y1) - side point of major axis
-        {
-            if (data[y1][x1] == 0)
-                continue;
-
-            for (int y2 = sidey2_start; y2 <= sidey2_fin; y2++)
-            {
-                for (int x2 = sidex2_start; x2 <= sidex2_fin; x2++) //point (x2, y2) - side point of major axis
-                {
-                    if (((y2 == y1) && x2 <= x1) || (data[y2][x2] == 0))
-                        continue;
-
-
-                    pair<int,int> center = compute_center(x1, y1, x2, y2);
-
-                    int x0 = center.first;
-                    int y0 = center.second;
-                    double a_sq = major_axis_length_sq(x1, y1, x2, y2); //major axis squared (half)
-                    double a = sqrt(a_sq);
-
-                    if (a < min_dist)
-                        continue;
-
-                    double orient = orientation(x1, y1, x2, y2);
-
-
-                    for (int y = ystart; y < yfin; y++)
-                    {
-                        for (int x = xstart; x < xfin; x++) //point of the ellipse (x,y)
-                        {
-                            if (((x == x1) && (y == y1)) || ((x == x2) && (y == y2)) || (data[y][x] == 0))
-                                continue;
-
-                            double d_sq = distance_square(x, y, x0, y0);
-                            double d = sqrt(d_sq); //distance from the center
-
-                            if ((d < min_dist) || (d > a))
-                                continue;
-
-                            double f_sq = min(distance_square(x, y, x1, y1),
-                                              distance_square(x, y, x2, y2)); //distance to the nearest side point
-                            double costau = (a_sq + d_sq - f_sq) / (2 * a * d);
-                            double costau_sq = costau * costau;
-                            if (costau_sq > 1)
-                            {
-                                costau_sq = 1;
-                            }
-                            if (costau_sq < 0) {
-                                costau_sq = 0;
-                            }
-                            double sintau_sq = 1 - costau_sq;
-                            double b_sq = (a_sq * d_sq * sintau_sq) / (a_sq - d_sq * costau_sq);
-                            double b = sqrt(b_sq);
-
-                            int b_round = cvRound(b);
-                            int b_ceil = cvCeil(b);
-                            int b_floor = cvFloor(b);
-                            double part = b - b_floor;
-
-                            if (b > min_dist) {
-                                acc[b_ceil] += cvCeil(data[y][x] * (1 - part));
-                                acc[b_floor] += cvCeil(data[y][x] * part);
-                            }
-
-                            if (b > min_dist) { //voting
-                                acc[b_round] += data[y][x];
-                            }
-                        }
-                    }
-
-                    int maxlength = 0;
-                    int maxvote = 0;
-                    for (int k = 0; k < maxlen; ++k) {
-                        if (acc[k] >= maxvote)
-                        {
-                            maxvote = acc[k];
-                            maxlength = k;
-                        }
-                        acc[k] = 0;
-                    }
-
-                    if ((maxvote > min_vote)  )
-                    {
-                        //we found something
-                        res.push_back(make_pair(ellipse_data(x0, y0, a, maxlength, orient, x1, y1, x2, y2), maxvote));
-                    }
-                }
-            }
-        }
-    }
-    pair<ellipse_data, int> e = *max_element(res.begin(), res.end(), compr2);
-    return e.first;
-}
-
-
-
 
 void clear_picture(Mat& edges, ellipse_data elp)
 {
